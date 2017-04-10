4b9013b6c4d25c9105b3e5b6eac0da820728c886
Hans Jakob Bollinger <contact@webbear.ch>
2017-04-10 11:38:10 +0200

    better input styles, improved mobile nav toggle function, overflow-x hidden on main-column Nicer mobile-language buttons styles

--- source/js/kit.js
+++ source/js/kit.js
@@ -1,6 +1,16 @@
 jQuery(document).ready(function($) {
-	$('.mobile-menu-toggle').on('click touch', function(){
+	$('.mobile-menu-toggle').on('click touch', function(e){
 		$(this).toggleClass('on');
+		$('body').toggleClass('mobile-nav-on');
 		$('.mobile-navigation').toggle(100);
+		e.stopPropagation();
 	});
+	// close mobile menu tapping outside toggle button
+	$('.body-container').on('click touch', function(e) {
+		$('body').removeClass('mobile-nav-on');
+		$('.mobile-menu-toggle').removeClass('on');
+		$('.mobile-navigation').hide();
+		e.stopPropagation();
+				
+	});	
 });
